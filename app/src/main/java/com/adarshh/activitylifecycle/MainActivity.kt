371f4f38282416a69aedac8da5f8c49491770fa9
Deb Adarsh <devadarsh4900@gmail.com>
2021-10-22 02:23:28 +0530

    Avengers App

---
+++ app/src/main/java/com/adarshh/activitylifecycle/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.adarshh.activitylifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scrollview_example)
+        println("OnCreate Called")
+        title="The Avengers"
+
+    }
+
+}
